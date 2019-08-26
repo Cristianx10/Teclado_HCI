0808709a511387fb3aa7ec0b09d53cbb58a95c85
Cristian Salguero <david_salinasx10@hotmail.com>
2019-08-26 13:44:00 -0500

    Organizacion del proyecto

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+
+var app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function(request, response) {
+  response.sendfile("/index.html");
+});
+
+app.listen(process.env.PORT || 1234);
+/*
+app.listen(3000, function() {
+    console.log("Escuchando en el puesto 3000");
+  });*/
